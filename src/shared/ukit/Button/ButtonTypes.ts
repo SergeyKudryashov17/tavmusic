ced446f5a3aceecfbe92c30b3686f865dcb7d225
Sergey Kudryashov <sergey.alex.kudryashov@yandex.ru>
2024-09-02 00:06:38 +0300

    Реализована компонент Button

---
+++ src/shared/ukit/Button/ButtonTypes.ts
@@ -0,0 +1,16 @@
+export type ButtonPropsType = {
+    children: React.ReactNode | React.ReactElement | string,
+    isFullView?: boolean,
+    isPrimary?: boolean,
+    isCenter: boolean,
+    onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void,
+    size: ButtonSize,
+    className?: string
+}
+
+export enum ButtonSize {
+    S = 's',
+    M = 'm',
+    L = 'l',
+    XL = 'xl'
+}

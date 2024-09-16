b135a4a8874e5d1b223292d41ef1d0c17ea26409
Sergey Kudryashov <sergey.alex.kudryashov@yandex.ru>
2024-09-16 22:14:41 +0300

    Добавлены новые компоненты иконок

--- src/shared/icons/index.ts
+++ src/shared/icons/index.ts
@@ -2,10 +2,19 @@ import { iconType } from "./iconType";
 import { MailIcon } from "./MailIcon/MailIcon";
 import { PhoneIcon } from "./PhoneIcon/PhoneIcon";
 import { ClockIcon } from "./ClockIcon/ClockIcon";
+import { PackageIcon } from "./PackageIcon/PackageIcon";
+import { HandshakeIcon } from "./HandshakeIcon/HandshakeIcon";
+import { ThumbsUpIcon } from "./ThumbsUpIcon/ThumbsUpIcon";
+import { PaletteIcon } from "./PaletteIcon/PaletteIcon";
 
 export {
     MailIcon,
     PhoneIcon,
     ClockIcon,
+    PackageIcon,
+    HandshakeIcon,
+    ThumbsUpIcon,
+    PaletteIcon,
+    
     type iconType
 }

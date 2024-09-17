99f261762b5e1a6f12dca9153b6d87fa9fa74dd7
Sergey Kudryashov <sergey.alex.kudryashov@yandex.ru>
2024-09-18 01:19:17 +0300

    Реализована сущность Brand

---
+++ src/entities/Brand/data/brandList.ts
@@ -0,0 +1,53 @@
+import { IBrand } from "../types/IBrand";
+
+import brand1 from '@/shared/assets/img/brands/1010music.jpg';
+import brand2 from '@/shared/assets/img/brands/10mfan.jpg';
+import brand3 from '@/shared/assets/img/brands/2_many_synths.jpg';
+import brand4 from '@/shared/assets/img/brands/2box.jpg';
+import brand5 from '@/shared/assets/img/brands/4ms.jpg';
+import brand6 from '@/shared/assets/img/brands/64_audio.jpg';
+import brand7 from '@/shared/assets/img/brands/9_solutions.jpg';
+import brand8 from '@/shared/assets/img/brands/a_b_rosin.jpg';
+
+export const brandList: IBrand[] = [
+    {
+        id: 0,
+        title: '1010MUSIC',
+        picture: brand1
+    },
+    {
+        id: 1,
+        title: '10MFAN',
+        picture: brand2
+    },
+    {
+        id: 2,
+        title: '2 MANY SYNTHS',
+        picture: brand3
+    },
+    {
+        id: 3,
+        title: '2BOX',
+        picture: brand4
+    },
+    {
+        id: 4,
+        title: '4MS',
+        picture: brand5
+    },
+    {
+        id: 5,
+        title: '64 AUDIO',
+        picture: brand6
+    },
+    {
+        id: 6,
+        title: '9.SOLUTIONS',
+        picture: brand7
+    },
+    {
+        id: 7,
+        title: 'A.B. ROSIN',
+        picture: brand8
+    }
+];

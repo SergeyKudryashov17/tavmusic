e57d5e1f958eec99aa074491d0d16702f4c4d366
Sergey Kudryashov <sergey.alex.kudryashov@yandex.ru>
2024-09-17 22:27:39 +0300

    Реализована сушность Product

---
+++ src/entities/Product/data/BestProductsList.ts
@@ -0,0 +1,43 @@
+import { IProduct } from "../types/IProduct";
+
+import img from '@/shared/assets/img/761876.518x0@2x.jpg';
+
+export const bestProductList: IProduct[] = [
+    {
+        id: 0,
+        name: 'Warwick Alien Deluxe 6 NT',
+        cost: 234430,
+        image: img
+    },
+    {
+        id: 1,
+        name: 'Warwick Alien Deluxe 6 NT',
+        cost: 134430,
+        image: img
+    },
+    {
+        id: 2,
+        name: 'Warwick Alien Deluxe 6 NT',
+        cost: 34430,
+        image: img
+    },
+    {
+        id: 3,
+        name: 'Warwick Alien Deluxe 6 NT',
+        cost: 4430,
+        image: img
+    },
+    {
+        id: 4,
+        name: 'Warwick Alien Deluxe 6 NT',
+        cost: 40430,
+        image: img
+    },
+    ,
+    {
+        id: 5,
+        name: 'Warwick Alien Deluxe 6 NT',
+        cost: 60430,
+        image: img
+    }
+] 
---
+++ src/entities/Product/index.ts
@@ -0,0 +1,6 @@
+import { IProduct } from "./types/IProduct";
+
+import { bestProductList } from "./data/BestProductsList";
+
+export { bestProductList };
+export type { IProduct };

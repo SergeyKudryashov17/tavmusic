7435b6a99328c92346b4b13bbc33ca94f815acd1
Sergey Kudryashov <sergey.alex.kudryashov@yandex.ru>
2024-09-17 22:34:50 +0300

    На главной странице добавлен раздел Лучшие предложения

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,10 +1,13 @@
 import BigSlider from "@/features/BigSlider";
 import ListBenefits from "@/features/ListBenefits";
+import ProductSlider from "@/features/ProductsSlider";
 import Container from "@/shared/ukit/Container";
 import Headline, { HeadlineViews } from "@/shared/ukit/Headline";
 import Section, { SectionViews } from "@/shared/ukit/Section";
 import GridCategories from "@/widgets/GridCategories";
 import Header from "@/widgets/Header";
+
+import { bestProductList } from '@/entities/Product/data/BestProductsList';
 
 const MainPage = () => {
     return (
@@ -30,6 +33,15 @@ const MainPage = () => {
                         Наши преимущества
                     </Headline>
                     <ListBenefits />
+                </Container>
+            </Section>
+
+            <Section>
+                <Container>
+                    <Headline view={HeadlineViews.Section} style={{ marginBottom: '5px' }}>
+                        Лучшие предложения
+                    </Headline>
+                    <ProductSlider productList={bestProductList} />
                 </Container>
             </Section>
         </>

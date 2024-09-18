ec6e107358871d7f102e967d232543770525d3c0
Sergey Kudryashov <sergey.alex.kudryashov@yandex.ru>
2024-09-19 00:55:55 +0300

    Небольшие корректировки верстки в MainPage

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,6 +1,6 @@
 import BigSlider from "@/features/BigSlider";
 import ListBenefits from "@/features/ListBenefits";
-import ProductSlider from "@/features/ProductsSlider";
+import ProductsSlider from "@/features/ProductsSlider";
 import Container from "@/shared/ukit/Container";
 import Headline, { HeadlineViews } from "@/shared/ukit/Headline";
 import Section, { SectionViews } from "@/shared/ukit/Section";
@@ -21,65 +21,67 @@ const MainPage = () => {
         <>
             <Header />
 
-            <BigSlider />
+            <main>
+                <BigSlider />
 
-            <Section>
-                <Container>
-                    <Headline view={HeadlineViews.Section}>
-                        Категории товаров
-                    </Headline>
-                    <GridCategories />
-                </Container>
-            </Section>
+                <Section>
+                    <Container>
+                        <Headline view={HeadlineViews.Section}>
+                            Категории товаров
+                        </Headline>
+                        <GridCategories />
+                    </Container>
+                </Section>
 
-            <Section view={SectionViews.Gray}>
-                <Container>
-                    <Headline view={HeadlineViews.Section}>
-                        Наши преимущества
-                    </Headline>
-                    <ListBenefits />
-                </Container>
-            </Section>
+                <Section view={SectionViews.Gray}>
+                    <Container>
+                        <Headline view={HeadlineViews.Section}>
+                            Наши преимущества
+                        </Headline>
+                        <ListBenefits />
+                    </Container>
+                </Section>
 
-            <Section>
-                <Container>
-                    <Headline view={HeadlineViews.Section} style={{ marginBottom: '5px' }}>
-                        Лучшие предложения
-                    </Headline>
-                    <ProductSlider productList={bestProductList} />
-                </Container>
-            </Section>
+                <Section>
+                    <Container>
+                        <Headline view={HeadlineViews.Section} style={{ marginBottom: '5px' }}>
+                            Лучшие предложения
+                        </Headline>
+                        <ProductsSlider productList={bestProductList} />
+                    </Container>
+                </Section>
 
-            <Section view={SectionViews.Gray}>
-                <Container>
-                    <Headline view={HeadlineViews.Section} style={{ marginBottom: '5px' }}>
-                        В наличии в Москве
-                    </Headline>
-                    <ProductSlider productList={stockMoscow} />
-                </Container>
-            </Section>
+                <Section view={SectionViews.Gray}>
+                    <Container>
+                        <Headline view={HeadlineViews.Section} style={{ marginBottom: '5px' }}>
+                            В наличии в Москве
+                        </Headline>
+                        <ProductsSlider productList={stockMoscow} />
+                    </Container>
+                </Section>
 
-            <Section>
-                <Container>
-                    <Headline view={HeadlineViews.Section} style={{ marginBottom: '5px' }}>
-                        Бренды
-                    </Headline>
-                    <BrandSlider brandList={brandList} />
-                </Container>
-            </Section>
+                <Section>
+                    <Container>
+                        <Headline view={HeadlineViews.Section} style={{ marginBottom: '5px' }}>
+                            Бренды
+                        </Headline>
+                        <BrandSlider brandList={brandList} />
+                    </Container>
+                </Section>
 
-            <Section view={SectionViews.Gray}>
-                <Container>
-                    <Headline view={HeadlineViews.Section}>
-                        Блог
-                    </Headline>
-                    <ArticleList 
-                        visibleCount={3} 
-                        list={articleList} 
-                        view={ArticleListView.Column}
-                    />
-                </Container>
-            </Section>
+                <Section view={SectionViews.Gray}>
+                    <Container>
+                        <Headline view={HeadlineViews.Section}>
+                            Блог
+                        </Headline>
+                        <ArticleList 
+                            visibleCount={3} 
+                            list={articleList} 
+                            view={ArticleListView.Column}
+                        />
+                    </Container>
+                </Section>
+            </main>
 
             <Footer />
         </>

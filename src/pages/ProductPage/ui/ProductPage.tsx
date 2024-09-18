4b597e424be763e70a138621105a25ef39f75ee1
Sergey Kudryashov <sergey.alex.kudryashov@yandex.ru>
2024-09-19 00:57:56 +0300

    Начата реализация страницы товара

--- src/pages/ProductPage/ui/ProductPage.tsx
+++ src/pages/ProductPage/ui/ProductPage.tsx
@@ -1,4 +1,33 @@
+import { bestProductList } from "@/entities/Product";
+import Container from "@/shared/ukit/Container";
+import Headline, { HeadlineViews } from "@/shared/ukit/Headline";
+import Footer from "@/widgets/Footer";
+import Header from "@/widgets/Header";
+import ProductsSlider from "@/features/ProductsSlider";
+import Section from "@/shared/ukit/Section";
+
 const ProductPage = () => {
-    return 'Товар';
+    return (
+        <>
+            <Header />
+
+            <main>
+                <Container >
+                    <div>Хлебные крошки</div>
+                    <div>Блок с информацией о товаре</div>
+                    <div>Вкладки</div>
+
+                    <Section>
+                        <Headline view={HeadlineViews.H4}>
+                            Смотрите также
+                        </Headline>
+                        <ProductsSlider productList={bestProductList} />
+                    </Section>
+                </Container>
+            </main>
+
+            <Footer />
+        </>
+    );
 }
 export default ProductPage;

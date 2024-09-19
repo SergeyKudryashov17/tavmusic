62a9a7185316a134c21ba0879d9ea366d108554f
Sergey Kudryashov <sergey.alex.kudryashov@yandex.ru>
2024-09-20 01:29:10 +0300

    Обновлен список товаров

--- src/entities/Product/data/stockMoscow.ts
+++ src/entities/Product/data/stockMoscow.ts
@@ -1,47 +1,4 @@
 import { IProduct } from "../types/IProduct";
-import img from '@/shared/assets/img/761876.518x0@2x.jpg';
+import { productList } from "./productList";
 
-export const stockMoscow: IProduct[] = [
-    {
-        id: 0,
-        name: 'Warwick Alien Deluxe 6 NT',
-        cost: 234430,
-        image: img,
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sollicitudin arcu nec arcu eleifend, vitae.'
-    },
-    {
-        id: 1,
-        name: 'Warwick Alien Deluxe 6 NT',
-        cost: 134430,
-        image: img,
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sollicitudin arcu nec arcu eleifend, vitae.'
-    },
-    {
-        id: 2,
-        name: 'Warwick Alien Deluxe 6 NT',
-        cost: 34430,
-        image: img,
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sollicitudin arcu nec arcu eleifend, vitae.'
-    },
-    {
-        id: 3,
-        name: 'Warwick Alien Deluxe 6 NT',
-        cost: 4430,
-        image: img,
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sollicitudin arcu nec arcu eleifend, vitae.'
-    },
-    {
-        id: 4,
-        name: 'Warwick Alien Deluxe 6 NT',
-        cost: 40430,
-        image: img,
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sollicitudin arcu nec arcu eleifend, vitae.'
-    },
-    {
-        id: 5,
-        name: 'Warwick Alien Deluxe 6 NT',
-        cost: 60430,
-        image: img,
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sollicitudin arcu nec arcu eleifend, vitae.'
-    }
-]
+export const stockMoscow: IProduct[] = productList;

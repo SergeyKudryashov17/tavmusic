e5c5e22c4330a9352ae7a9fde0b34513b137c2db
Sergey Kudryashov <sergey.alex.kudryashov@yandex.ru>
2024-09-19 23:22:12 +0300

    Реализована страница Product

--- src/pages/ProductPage/ui/ProductPage.tsx
+++ src/pages/ProductPage/ui/ProductPage.tsx
@@ -5,17 +5,24 @@ import Footer from "@/widgets/Footer";
 import Header from "@/widgets/Header";
 import ProductsSlider from "@/features/ProductsSlider";
 import Section from "@/shared/ukit/Section";
+import ProductInfo from "@/widgets/ProductInfo";
+import { getIDfromURL } from "@/shared/utils";
 
 const ProductPage = () => {
+    const productID = getIDfromURL();
+
     return (
         <>
             <Header />
 
             <main>
-                <Container >
-                    <div>Хлебные крошки</div>
-                    <div>Блок с информацией о товаре</div>
-                    <div>Вкладки</div>
+                <Container>
+                    <Section>
+                        { productID !== null 
+                            ? ( <ProductInfo productID={Number(productID)}/> ) 
+                            : ( <div>Нет такого товара</div> )
+                        }
+                    </Section>
 
                     <Section>
                         <Headline view={HeadlineViews.H4}>
